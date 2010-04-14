72266c3205070e4d6be2387ed0785d6b02acd07e
Jochen <jochen@zum-quadrat.de>
2010-04-14 15:29:15 +0200

    Set GPLv2 als license for gsm-ussd

--- gsm-ussd.pl
+++ gsm-ussd.pl
@@ -1,10 +1,28 @@
 #!/usr/bin/perl
 ########################################################################
 # vim: set expandtab sw=4 ts=4 ai nu: 
+########################################################################
+# Copyright (C) 2010 Jochen Gruse, jochen@zum-quadrat.de
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
+########################################################################
 
 use strict;
 use warnings;
-use 5.008;                  # Encode only vailable since 5.8
+use 5.008;                  # Encode::GSM0338 only vailable since 5.8
 
 use Getopt::Long;
 use Pod::Usage;
